56170cee703e9495bc0b4cba3eafd799bde0e8cd
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-06 21:15:05 +0530

    create express app server in app.js, intall dotenv and listen express app server on port from env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world! This is a test Code.");
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.listen(PORT, () => {
+  console.log(`🌐 Server is running on URL http://127.0.0.1:${PORT}`);
+});
