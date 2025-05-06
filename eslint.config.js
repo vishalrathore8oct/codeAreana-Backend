1ae3481a65de701def1ec53fa55630cb2c79b415
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-06 18:14:16 +0530

    Install eslint, intilize eslint, Install ESLint + Prettier Integration Plugins, and Update eslint.config.js file and Add eslint format scripts in package.json

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["node_modules", "dist", "build"],
+
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+]);
