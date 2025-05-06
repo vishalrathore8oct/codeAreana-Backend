baba6a20528835b2698397b45905d0d7234072d6
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-06 21:43:49 +0530

    install cors, cookie-parse and use them as middleware with josn and urlencoded middlwares in express app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import logger from "./middlewares/logger.middleware.js";
 import { swaggerUi, swaggerSpec } from "./middlewares/swagger.middleware.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -27,6 +29,12 @@ app.use(
 
 // Middleware to Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// add all essential middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 
 // Default Global error handler
 app.use(errorHandler);
