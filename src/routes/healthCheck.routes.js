5171228df1b77d2fe40c7b99bc87964e518d85a6
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-08 11:55:43 +0530

    add a Swagger UI comment for your /health route

--- src/routes/healthCheck.routes.js
+++ src/routes/healthCheck.routes.js
@@ -3,6 +3,24 @@ import { healthCheck } from "../controllers/healthCheck.controllers.js";
 
 const healthCheckRouter = Router();
 
+/**
+ * @swagger
+ * /api/v1/health-check:
+ *   get:
+ *     summary: Health Check
+ *     description: Returns the health status of the application.
+ *     responses:
+ *       200:
+ *         description: Application is healthy.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: "OK"
+ */
 healthCheckRouter.get("/", healthCheck);
 
 export default healthCheckRouter;
