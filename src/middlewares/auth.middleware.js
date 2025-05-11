42a04c330207ac9b591e865e13a3c4969d443563
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-11 16:45:19 +0530

    create a isLoggedIn access token auth middleware for check user login

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const isLoggedIn = (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized! No token provided." });
+  }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  if (!decodedToken) {
+    return res.status(401).json({ message: "Unauthorized! Invalid token." });
+  }
+  console.log("Decoded token:", decodedToken);
+
+  req.user = decodedToken;
+  next();
+};
