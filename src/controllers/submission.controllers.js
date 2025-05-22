56513d2a8db05aaca67235139dbb01b6be272ffd
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-22 10:43:15 +0530

    complete the getAllUserSubmissions controller business logic code

--- src/controllers/submission.controllers.js
+++ src/controllers/submission.controllers.js
@@ -5,7 +5,25 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { prisma } from "../config/prismaClient.js";
 
-export const getAllUserSubmissions = asyncHandler(async (req, res) => {});
+export const getAllUserSubmissions = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const submissions = await prisma.submission.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+  if (!submissions) {
+    return ApiResponse.error(
+      res,
+      new ApiError(404, "No submissions found for this user"),
+    );
+  }
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Submissions fetched successfully", submissions),
+    );
+});
 
 export const getUserSubmissionsForProblem = asyncHandler(
   async (req, res) => {},
