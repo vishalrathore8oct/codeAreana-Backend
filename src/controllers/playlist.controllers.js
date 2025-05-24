5acce32748450661410617c2fa58c930c79db4ca
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-24 11:37:55 +0530

    complete playlist CURD controllers stater template code

---
+++ src/controllers/playlist.controllers.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { prisma } from "../config/prismaClient.js";
+
+export const createPlaylist = asyncHandler(async (req, res) => {});
+
+export const deletePlaylist = asyncHandler(async (req, res) => {});
+
+export const getAllPlaylistDetails = asyncHandler(async (req, res) => {});
+
+export const getPlaylistDetails = asyncHandler(async (req, res) => {});
+
+export const addProblemToPlaylist = asyncHandler(async (req, res) => {});
+
+export const removeProblemFromPlaylist = asyncHandler(async (req, res) => {});
