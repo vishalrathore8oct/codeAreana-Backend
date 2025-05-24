00fa9b5957bfdd5bd4009025bfc2ad15db6b93c9
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-05-24 12:24:12 +0530

    complete the controller logic of create-playlist controller

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -4,7 +4,34 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { prisma } from "../config/prismaClient.js";
 
-export const createPlaylist = asyncHandler(async (req, res) => {});
+export const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const userId = req.user.userId;
+
+  const existingPlaylist = await prisma.playlist.findFirst({
+    where: {
+      name,
+      userId,
+    },
+  });
+  if (existingPlaylist) {
+    throw new ApiError(400, "Playlist with this name already exists.");
+  }
+
+  const playlist = await prisma.playlist.create({
+    data: {
+      name,
+      description,
+      userId,
+    },
+  });
+
+  res.status(201).json(
+    new ApiResponse(201, "Playlist created successfully.", {
+      playlist,
+    }),
+  );
+});
 
 export const deletePlaylist = asyncHandler(async (req, res) => {});
 
